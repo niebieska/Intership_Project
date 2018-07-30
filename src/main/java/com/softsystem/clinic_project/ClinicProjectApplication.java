b55f9de195ec74cd332643fdb28dc1aa78a77dcb
Kacper <kacpuu@gmail.com>
2018-07-30 09:47:54 +0200

    init

---
+++ src/main/java/com/softsystem/clinic_project/ClinicProjectApplication.java
@@ -0,0 +1,12 @@
+package com.softsystem.clinic_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicProjectApplication.class, args);
+	}
+}
