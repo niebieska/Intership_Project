9fe2d5d8b8850a7470b5a3d8253fa69e735bd05e
Unknown <lukaszptaszek@interia.eu>
2018-07-31 08:21:48 +0300

    New App

---
+++ src/main/java/com/softsystem/clinic/project/controller/Controller.java
@@ -0,0 +1,27 @@
+package com.softsystem.clinic.project.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+public class Controller {
+
+	
+	@GetMapping(value="/welcome")
+	public String hello() {
+		return "Hello world";
+	}
+	
+	
+}
