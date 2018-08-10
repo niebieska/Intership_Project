d517da75bbbe1349771b6710e360f67765cbced0
megamen128 <megamen1285@gmail.com>
2018-08-10 13:19:41 +0200

    Schedule services

--- src/main/java/com/softsystem/clinic/project/controller/ScheduleController.java
+++ src/main/java/com/softsystem/clinic/project/controller/ScheduleController.java
@@ -1,14 +1,26 @@
 package com.softsystem.clinic.project.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.softsystem.clinic.project.dao.ScheduleRepository;
+import com.softsystem.clinic.project.model.Doctor;
+import com.softsystem.clinic.project.model.Patient;
 import com.softsystem.clinic.project.model.Schedule;
 import com.softsystem.clinic.project.services.ScheduleService;
 import com.softsystem.clinic.project.services.ScheduleServiceImpl;
@@ -32,6 +44,29 @@ public class ScheduleController {
 	public List<Schedule> getAllSchedule(@PathVariable String id)
 	{
 		return scheduleServ.findOne(id);
+	}
+	
+	@DeleteMapping(value="/deleteschedule/{id}")
+	private void delete(@PathVariable int id) {
+		scheduleRepo.deleteById(id);
+	}
+
+	
+	@PutMapping("/updateschedule/{id}")
+	public ResponseEntity<Object> updateSchedule(@RequestBody Schedule schedule, @PathVariable int id) {
+
+		Optional<Schedule> scheduleOptional =scheduleRepo.findById(id);
+		if (!scheduleOptional.isPresent()) 
+			return ResponseEntity.notFound().build();
+
+		schedule.setId(id);
+		scheduleRepo.save(schedule);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping(value="/saveschedule")
+	public  void saveSchedule(@RequestBody Schedule schedule){
+		scheduleRepo.save(schedule);
 	}
 
 }
