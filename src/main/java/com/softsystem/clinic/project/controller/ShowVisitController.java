ff29aca4744af92a2a9466b4258536adf50ddf56
hoophoop93 <michalikos17@wp.pl>
2018-08-10 13:04:16 +0200

    Added Visit View and VisitController.

--- src/main/java/com/softsystem/clinic/project/controller/ShowVisitController.java
+++ src/main/java/com/softsystem/clinic/project/controller/ShowVisitController.java
@@ -10,46 +10,49 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.softsystem.clinic.project.dao.PatientRepository;
-import com.softsystem.clinic.project.model.Patient;
+import com.softsystem.clinic.project.dao.VisitRepository;
 import com.softsystem.clinic.project.model.Reception;
+import com.softsystem.clinic.project.model.Visit;
 import com.softsystem.clinic.project.services.CurrentReceptionistService;
 
 @Controller
-public class ShowPatientsController {
+public class ShowVisitController {
 
-	@Autowired
-	private PatientRepository patientRepository;
-	
-	
 	@Inject
 	CurrentReceptionistService currentReceptionService;
+	@Autowired
+	private VisitRepository visitRepository;
 	
-	
-	@GetMapping(value="/showPatients")
+
+	@GetMapping(value = "/showVisits")
 	public ModelAndView showAllPatients(Model model) {
 		ModelAndView modelAndView = new ModelAndView();
-		
+
 		if (currentReceptionService.isAuthenticated()) {
 			// if logged in it passes to the patient page
 			System.out.println("zalogowany");
-			modelAndView.setViewName("/showPatients");
+			modelAndView.setViewName("/showVisits");
 
 		} else {
 			// return to log in
 			System.out.println("niezalogowany");
-		 modelAndView.setViewName("redirect:/login");
+			modelAndView.setViewName("redirect:/login");
 		}
+
+		//List<Visit> visitList = (List<Visit>) visitRepository.findAllVis();
 		
-		
-		List<Patient> patientList =  (List<Patient>) patientRepository.findAll();
-		
+		//for(Visit visit: visitList) {
+			//System.out.println(visit.getVis_Room());
+		//}
+
 		Reception reception = currentReceptionService.getReception();
 		
 		
-		model.addAttribute("patientList",patientList);
-		model.addAttribute("reception",reception);
 		
+
+		//model.addAttribute("visitList", visitList);
+		model.addAttribute("reception", reception);
+
 		return modelAndView;
 	}
 }
